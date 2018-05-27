96e52372ff04a5b5f569be8600e2f6595a7d1382
Oğuzcan Köse <oguzcan.kose@hotmail.com>
2018-05-27 17:28:12 +0100

    removed static data

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -11,24 +11,7 @@ export default {
   },
   data() {
       return {
-          loadedPosts: {
-              isAdmin: true,
-              id: '1',
-              title: 'Nasıl Yapılır 1',
-              slug: 'nasil-yapilir',
-              kategori: 'teknoloji',
-              author: 'Oguzcan Kose',
-              file: '',
-              steps: {
-                  title: 'adim 1',
-                  descripton: 'açıklama 1'
-              },
-              meta: {
-                  name: 'Meta name',
-                  content: 'meta desc',
-                  keywords: 'meta keyword'
-              }
-          }
+          
       }
   }
 }
